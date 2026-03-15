fba65093bb08edad67242f223e52f4f733bcd3b5
Agent4580 <sashapetrov89198258034@gmail.com>
2026-03-15 12:44:19 +0400

    Реализация механики ползания по стенам

--- scripts/hero.gd
+++ scripts/hero.gd
@@ -13,17 +13,26 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	
 	#Определяем направление движения
-	var direction = Input.get_axis('move_left', 'move_right')
+	var direction_h = Input.get_axis('move_left', 'move_right')
 	
 	#Делаем движение, если direction не равен нулю
 	#иначе останавливаемся
-	if direction:
-		velocity.x = speed * direction
+	if direction_h:
+		velocity.x = speed * direction_h
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
 	
 	if Input.is_action_just_pressed('jump') and is_on_floor():
 		velocity.y = jump
 	
+	var direction_v = Input.get_axis('move_up', 'move_down')
+	
+	if  is_on_wall()  and not is_on_floor():
+		$Gravity.set_gravity(0)
+		velocity.y = 0
+		if direction_v:
+			velocity.y = speed * direction_v
+	else:
+		$Gravity.set_gravity(980)
 	#Обязательная строка для данной игры
 	move_and_slide()
